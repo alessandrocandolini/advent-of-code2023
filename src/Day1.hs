f9fd9c9722dcd39e2d77d5b9f3b9a8f1308ef36c
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-12-01 21:46:55 +0000

    day 1, part 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,11 +1,99 @@
 module Day1 where
+
+import Control.Applicative (Alternative (many))
+import Data.Char (digitToInt, isDigit)
+import Data.Functor (($>))
+import Data.List (find)
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as N
+import Data.Monoid ()
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Void (Void)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import Witherable (catMaybes, mapMaybe)
+import Data.Either.Combinators (rightToMaybe)
 
 program :: FilePath -> IO ()
 program = (=<<) print . fmap logic . T.readFile
 
-data Answer = Answer deriving (Eq, Show)
+data Answer = Answer Int Int deriving (Eq, Show)
+
+data Digit
+  = One
+  | Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  deriving (Eq, Show, Bounded, Enum)
+
+allDigits :: [Digit]
+allDigits = [minBound .. maxBound]
+
+toInt :: Digit -> Int
+toInt = (+ 1) . fromIntegral . fromEnum
+
+digitFromChar :: Char -> Maybe Digit
+digitFromChar c
+  | isDigit c = find ((==) (digitToInt c) . toInt) allDigits
+  | otherwise = Nothing
+
+toString :: Digit -> T.Text
+toString One = "one"
+toString Two = "two"
+toString Three = "three"
+toString Four = "four"
+toString Five = "five"
+toString Six = "six"
+toString Seven = "seven"
+toString Eight = "eight"
+toString Nine = "nine"
 
 logic :: T.Text -> Answer
-logic = const Answer
+logic = Answer <$> sum . part1 <*> sum . part2
+
+part1 :: T.Text -> [Int]
+part1 = processLines . parseLines parser1
+
+part2 :: T.Text -> [Int]
+part2 = processLines . parseLines parser2
+
+parseLines :: Parser [Digit] -> T.Text -> [NonEmpty Digit]
+parseLines p = mapMaybe parseLine . T.lines where
+  parseLine = (=<<) N.nonEmpty . rightToMaybe . P.parse p ""
+
+processLines :: [NonEmpty Digit] -> [Int]
+processLines = fmap processLine
+ where
+  processLine = read . foldMap (show . toInt) . tupleToList . firstAndLast
+  firstAndLast = (,) <$> N.head <*> N.last
+  tupleToList (a, b) = [a, b]
+
+type Parser = P.Parsec Void T.Text
+type ParserError = P.ParseErrorBundle T.Text Void
+
+emap :: (a -> Maybe b) -> Parser a -> Parser b
+emap f = (=<<) (failIfNothing . f)
+ where
+  failIfNothing (Just a) = pure a
+  failIfNothing Nothing = fail "error parsing"
+
+digitFromCharP :: Parser Digit
+digitFromCharP = emap digitFromChar P.digitChar
+
+digitFromNameP :: Parser Digit
+digitFromNameP = P.choice $ fmap (\d -> build (toString d) d) allDigits
+ where
+  build :: T.Text -> Digit -> Parser Digit
+  build s d = P.string s $> d
+
+parser1 :: Parser [Digit]
+parser1 = catMaybes <$> many (P.choice [Just <$> digitFromCharP, Nothing <$ P.anySingle])
+
+parser2 :: Parser [Digit]
+parser2 = catMaybes <$> many (P.choice [Just <$> digitFromCharP, Just <$> digitFromNameP, Nothing <$ P.anySingle])
--- test/Day1Spec.hs
+++ test/Day1Spec.hs
@@ -1,21 +1,61 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day1Spec where
 
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Day1
+import NeatInterpolation
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
+import qualified Text.Megaparsec as P
+
+parsePart2 :: T.Text -> Either ParserError [Digit]
+parsePart2 = P.parse parser2 ""
 
 spec :: Spec
 spec = describe "Day 1" $ do
-  it ""
-    $ 1
-    `shouldBe` 1
+  it "logic"
+    $ part1
+      [trimming|1abc2
+      pqr3stu8vwx
+      a1b2c3d4e5f
+      treb7uchet|]
+    `shouldBe` [12, 38, 15, 77]
 
-  prop ""
-    $ \l -> reverse (reverse l) == (l :: [Int])
+  it "parsePart2 example 1"
+    $ parsePart2 "123"
+    `shouldBe` Right [One, Two, Three]
 
-  xit "solve the puzzle" $ do
-     input <- T.readFile "resources/input1"
-     logic input `shouldBe` Answer
+  it "parsePart2 example 2"
+    $ parsePart2 "1a2b3"
+    `shouldBe` Right [One, Two, Three]
+
+  it "parsePart2 example 3"
+    $ parsePart2 "one2twone"
+    `shouldBe` Right [One, Two, Two]
+
+  it "parsePart2 example 4"
+    $ parsePart2 "aoneb2ttwone"
+    `shouldBe` Right [One, Two, Two]
+  it "parsePart2 example 5"
+    $ parsePart2 "aoneon2eighthree"
+    `shouldBe` Right [One, Two, Eight]
+
+  it "part2"
+    $ part2
+      [trimming|
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+      |]
+    `shouldBe` [29, 83, 13, 24, 42, 14, 76]
+
+  it "solve the puzzle" $ do
+    input <- T.readFile "resources/input1"
+    logic input `shouldBe` (Answer 55712 55445)
