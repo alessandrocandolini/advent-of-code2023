78f9f9c36cc796788a77559029de6de9f1ab5ad2
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-12-04 21:39:28 +0000

    some useless preliminary stuff for day 3

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Day3 where
+
+import Control.Applicative ((<|>), Alternative (many))
+import Data.Char (isDigit, digitToInt)
+import Data.Functor (($>))
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.Vector as V
+import Parser
+import qualified Witherable as W
 
 program :: FilePath -> IO ()
 program = (=<<) print . fmap logic . T.readFile
@@ -9,3 +18,27 @@ data Answer = Answer deriving (Eq, Show)
 
 logic :: T.Text -> Answer
 logic = const Answer
+
+data GridElement
+  = Digit Int
+  | Blank
+  | Symbol Char
+  deriving (Eq, Show)
+
+data Grid a = Grid
+  { gridWidth :: Int
+  , gridHeight :: Int
+  , content :: V.Vector a
+  }
+  deriving (Eq, Show, Functor)
+
+gridElementP :: Parser GridElement
+gridElementP = digitP <|> blankP <|> symbolP
+ where
+  blankP = char '.' $> Blank
+  digitP = Digit . digitToInt <$> digit
+  symbolP = (fmap Symbol . W.filter (\c -> c /= '.' && c /= '\n' && (not . isDigit) c)) anyChar
+
+gridElementsP :: Parser [[GridElement]]
+gridElementsP = many gridElementP `sepBy` newline
+
--- test/Day3Spec.hs
+++ test/Day3Spec.hs
@@ -1,21 +1,51 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day3Spec where
 
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Day3
+import NeatInterpolation (trimming)
+import Parser (parseAll)
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
 
 spec :: Spec
 spec = describe "Day 3" $ do
-  it ""
-    $ 1
-    `shouldBe` 1
+  it "parse grid"
+    $ parseAll
+      gridElementsP
+      ( T.unpack
+          [trimming|
+        467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..
+        |]
+      )
+    `shouldBe` Just
+      [ [Digit 4, Digit 6, Digit 7, Blank, Blank, Digit 1, Digit 1, Digit 4, Blank, Blank]
+      , [Blank, Blank, Blank, Symbol '*', Blank, Blank, Blank, Blank, Blank, Blank]
+      , [Blank, Blank, Digit 3, Digit 5, Blank, Blank, Digit 6, Digit 3, Digit 3, Blank]
+      , [Blank, Blank, Blank, Blank, Blank, Blank, Symbol '#', Blank, Blank, Blank]
+      , [Digit 6, Digit 1, Digit 7, Symbol '*', Blank, Blank, Blank, Blank, Blank, Blank]
+      , [Blank, Blank, Blank, Blank, Blank, Symbol '+', Blank, Digit 5, Digit 8, Blank]
+      , [Blank, Blank, Digit 5, Digit 9, Digit 2, Blank, Blank, Blank, Blank, Blank]
+      , [Blank, Blank, Blank, Blank, Blank, Blank, Digit 7, Digit 5, Digit 5, Blank]
+      , [Blank, Blank, Blank, Symbol '$', Blank, Symbol '*', Blank, Blank, Blank, Blank]
+      , [Blank, Digit 6, Digit 6, Digit 4, Blank, Digit 5, Digit 9, Digit 8, Blank, Blank]
+      ]
 
   prop ""
     $ \l -> reverse (reverse l) == (l :: [Int])
 
   xit "solve the puzzle" $ do
-     input <- T.readFile "resources/input3"
-     logic input `shouldBe` Answer
+    input <- T.readFile "resources/input3"
+    logic input `shouldBe` Answer
