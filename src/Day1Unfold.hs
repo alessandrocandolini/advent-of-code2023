448cff2c2e6688d93e21db53e8dbef2bec8af0fa
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-12-08 21:51:00 +0000

    day 1 using unfold

---
+++ src/Day1Unfold.hs
@@ -0,0 +1,59 @@
+module Day1Unfold where
+
+import Data.Char (isDigit)
+import Data.List (unfoldr)
+import qualified Data.List.NonEmpty as N
+import Data.Tuple (swap)
+import Witherable (mapMaybe, Filterable (catMaybes))
+
+{- This module solves AOC 2023 Day 1 relying as much as possible on built-in types (ie, Char) and vanilla techniques.
+ - For a type safer solution (eg, modelling digits as ADT), that relies on parser combinators, see Day1.hs
+ -
+ - Explicit recursion is avoided, by relying on `tails` (of comonadic inspiration)
+ -}
+
+program :: FilePath -> IO ()
+program = (=<<) print . fmap solve . readFile
+
+solve :: String -> (Int, Int)
+solve input = (answer1, answer2)
+ where
+  answer1 = (sum . part1) input
+  answer2 = (sum . part2) input
+
+part1 :: String -> [Int]
+part1 =
+  fmap (read . firstAndLast)
+    . mapMaybe (N.nonEmpty . filter isDigit)
+    . lines
+
+part2 :: String -> [Int]
+part2 =
+  fmap (read . firstAndLast)
+    . mapMaybe (N.nonEmpty . parseAll)
+    . lines
+
+firstAndLast :: N.NonEmpty a -> [a]
+firstAndLast l = [N.head l, N.last l]
+
+parse :: String -> Maybe Char
+parse [] = Nothing
+parse ('o' : 'n' : 'e' : _) = Just '1'
+parse ('t' : 'w' : 'o' : _) = Just '2'
+parse ('t' : 'h' : 'r' : 'e' : 'e' : _) = Just '3'
+parse ('f' : 'o' : 'u' : 'r' : _) = Just '4'
+parse ('f' : 'i' : 'v' : 'e' : _) = Just '5'
+parse ('s' : 'i' : 'x' : _) = Just '6'
+parse ('s' : 'e' : 'v' : 'e' : 'n' : _) = Just '7'
+parse ('e' : 'i' : 'g' : 'h' : 't' : _) = Just '8'
+parse ('n' : 'i' : 'n' : 'e' : _) = Just '9'
+parse (c : _)
+  | isDigit c = Just c
+  | otherwise = Nothing
+
+parseAll :: String -> String
+parseAll = catMaybes . unfoldr step
+ where
+  step :: String -> Maybe (Maybe Char, String)
+  step [] = Nothing
+  step s = Just (parse s, drop 1 s)
---
+++ test/Day1UnfoldSpec.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Day1UnfoldSpec where
+
+import qualified Data.Text as T
+import Day1Unfold
+import NeatInterpolation ( trimming )
+import Test.Hspec ( describe, it, shouldBe, Spec )
+
+spec :: Spec
+spec = describe "Day 1" $ do
+  it "logic"
+    $ part1 (T.unpack
+      [trimming|1abc2
+      pqr3stu8vwx
+      a1b2c3d4e5f
+      treb7uchet|] )
+    `shouldBe` [12, 38, 15, 77]
+
+  it "parseAll example 1"
+    $ parseAll "123"
+    `shouldBe` ['1', '2', '3']
+
+  it "parseAll example 2"
+    $ parseAll "1a2b3"
+    `shouldBe` ['1', '2', '3']
+
+  it "parseAll example 3"
+    $ parseAll "one2twone"
+    `shouldBe` ['1', '2', '2', '1']
+
+  it "parseAll example 4"
+    $ parseAll "aoneb2ttwone"
+    `shouldBe` ['1', '2', '2', '1']
+
+  it "parseAll example 5"
+    $ parseAll "aoneon2eighthree"
+    `shouldBe` ['1', '2', '8', '3']
+
+  it "parseAll example 6"
+    $ parseAll "eightwothree"
+    `shouldBe` ['8', '2', '3']
+
+  it "parseAll example 6"
+    $ parseAll "threeeightwo"
+    `shouldBe` ['3', '8', '2']
+
+  it "parseAll example 7"
+    $ parseAll "oneighthreeighthreeightwone"
+    `shouldBe` ['1', '8', '3', '8', '3', '8', '2', '1']
+
+  it "part2"
+    $ part2 (T.unpack
+      [trimming|
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+      |])
+    `shouldBe` [29, 83, 13, 24, 42, 14, 76]
+
+  it "solve the puzzle" $ do
+    input <- readFile "resources/input1"
+    solve input `shouldBe` (55712, 55413)
